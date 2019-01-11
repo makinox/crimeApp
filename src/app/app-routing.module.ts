a4fed5d94e24d0382833bd8e44f8048b238c0c54
Jesús Bossa <me@jesusbossa.com>
2019-01-11 10:37:36 -0500

    [ADD] Routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from '../app/dashboard/dashboard.component'
+import {ListComponent} from '../app/list/list.component'
+import {DetailsComponent} from '../app/details/details.component'
 
 
 const routes: Routes = [
-  // {path: '/', component: }
+  {path: '', component: DashboardComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'detail', component: DetailsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
