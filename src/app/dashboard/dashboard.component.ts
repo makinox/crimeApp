69c8183349013bc6c4e3c09765413a25aea038c3
Jesús Bossa <me@jesusbossa.com>
2019-01-16 11:35:20 -0500

    [ADD] Chart working

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -31,10 +31,29 @@ export class DashboardComponent implements OnInit {
     credCount[0]['attributes']['count'],
     cardCount[0]['attributes']['count']
   ]
+  public lineChartData: Array<any> = [{ data: crimeData.map(e => e.attributes.count), label: '' }];
+  public lineChartLabels: Array<any> = crimeData.map(e => e.id);
+  public lineChartOptions: any = {responsive: true};
+  public lineChartColors: Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartType: string = 'line';
 
   constructor() { }
 
   async ngOnInit() {
+
+    // console.log(crimeData)
+    // console.log(crimeData.map(e => e.id))
+    // console.log(crimeData.map(e => e.attributes.count))
+
     // const headers = new Headers({
     //   "Authorization": "Token 1b52bedb-e675-41ee-aca1-9e5972182748",
     //   "Content-Type": "application/json"
@@ -54,59 +73,38 @@ export class DashboardComponent implements OnInit {
     if (e === 0) {
       this.nav = 0
       this.title = 'Crime servers'
-      console.log(crimeCount[0])
-      console.log(crimeData)
+      this.lineChartData = [{ data: crimeData.map(e => e.attributes.count), label: '' }];
+      this.lineChartLabels = crimeData.map(e => e.id);
     } else if (e === 1) {
       this.nav = 1
       this.title = 'Malware'
-      console.log(malwCount[0])
-      console.log(malwData)
+      this.lineChartData = [{ data: malwData.map(e => e.attributes.count), label: '' }];
+      this.lineChartLabels = malwData.map(e => e.id);
     } else if (e === 2) {
       this.nav = 2
       this.title = 'Bots'
-      console.log(botsCount[0])
-      console.log(botsData)
+      this.lineChartData = [{ data: botsData.map(e => e.attributes.count), label: '' }];
+      this.lineChartLabels = botsData.map(e => e.id);
     } else if (e === 3) {
       this.nav = 3
       this.title = 'Credentials'
-      console.log(credCount[0])
-      console.log(credData)
+      this.lineChartData = [{ data: credData.map(e => e.attributes.count), label: '' }];
+      this.lineChartLabels = credData.map(e => e.id);
     } else if (e === 4) {
       this.nav = 4
       this.title = 'Cards'
-      console.log(cardCount[0])
-      console.log(cardData)
+      this.lineChartData = [{ data: cardData.map(e => e.attributes.count), label: '' }];
+      this.lineChartLabels = cardData.map(e => e.id);
     }
 
   }
 
-  // lineChart
-  public lineChartData: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Crime' }
-  ];
-  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions: any = {
-    responsive: true
-  };
-  public lineChartColors: Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-  ];
-  public lineChartType: string = 'line';
+  // public chartClicked(e: any): void {
+  //   console.log(e);
+  // }
 
-  // events
-  public chartClicked(e: any): void {
-    console.log(e);
-  }
-
-  public chartHovered(e: any): void {
-    console.log(e);
-  }
+  // public chartHovered(e: any): void {
+  //   console.log(e);
+  // }
 
 }
